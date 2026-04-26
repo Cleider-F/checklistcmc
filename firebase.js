1718bbf348c466e3d2f629e130b7676aa1b7d0aa
Cleider-F <cleider1000@hotmail.com>
2026-04-26 10:49:04 -0300

    Add files via upload

--- firebase.js
+++ firebase.js
@@ -6,13 +6,16 @@ import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebas
 
 import { getStorage } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-storage.js";
 
-import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js"; // 🔥 FALTAVA ISSO
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyCRSVQ-M_r9RukOOZBWt5GZuoUQJFBHZ2A",
   authDomain: "controle-manutencao-cc96e.firebaseapp.com",
   projectId: "controle-manutencao-cc96e",
+
+  // 🔥 CORREÇÃO AQUI
   storageBucket: "controle-manutencao-cc96e.firebasestorage.app",
+
   messagingSenderId: "453313302773",
   appId: "1:453313302773:web:64e384e412c1458adcf5bc"
 };
@@ -22,4 +25,9 @@ const app = initializeApp(firebaseConfig);
 // 🔥 EXPORTS
 export const db = getFirestore(app);
 export const auth = getAuth(app);
-export const storage = getStorage(app);
+
+// 🔥 FORÇA O USO DO BUCKET CORRETO
+export const storage = getStorage(
+  app,
+  "gs://controle-manutencao-cc96e.firebasestorage.app"
+);
