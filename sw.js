5d321319d9ffa9908d028daaebbeb1a40284eee1
Cleider-F <cleider1000@hotmail.com>
2026-05-04 11:01:11 -0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,5 @@
+// Este arquivo pode ficar vazio por enquanto, 
+// mas é necessário para o navegador habilitar a instalação.
+self.addEventListener('fetch', function(event) {
+    // Aqui você poderia adicionar lógica de cache para funcionar offline
+});
