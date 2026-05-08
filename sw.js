58edf3c969acb0c5a6f29852d8ef9192e580807e
Cleider-F <cleider1000@hotmail.com>
2026-05-08 12:54:52 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,23 @@
-// Este arquivo pode ficar vazio por enquanto, 
-// mas é necessário para o navegador habilitar a instalação.
-self.addEventListener('fetch', function(event) {
-    // Aqui você poderia adicionar lógica de cache para funcionar offline
+const CACHE_NAME = "cmc-v1";
+
+self.addEventListener("install", event => {
+  console.log("SW instalado");
+
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll([
+        "/",
+        "/index.html",
+        "/navbar.css"
+      ]);
+    })
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    fetch(event.request).catch(() => {
+      return caches.match(event.request);
+    })
+  );
 });
